cf0d2be1499476a4355fa5556a06dc757876d545
Mahakaal101 <124979598+Mahakaal101@users.noreply.github.com>
2023-03-12 08:43:36 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -8,33 +8,35 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Var(object):
-    MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', "15823382"))
-    API_HASH = str(getenv('API_HASH', "016d5e115a06ddfb6121823d72ae4d8c"))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', "5499850853:AAF1saxsT9Q46H7zGs90asuTpAqND5qDgnI"))
+    MULTI_CLIENT = True
+    API_ID = int(getenv('API_ID', '15823382'))
+    API_HASH = str(getenv('API_HASH', '016d5e115a06ddfb6121823d72ae4d8c'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '5606869491:AAHJbj5U_fEbYmODiezZsqWP5lIWSJalQ6Y'))
+    MULTI_TOKEN1 = str(getenv('MULTI_TOKEN1', '5499850853:AAF1saxsT9Q46H7zGs90asuTpAqND5qDgnI'))
+    MULTI_TOKEN2 = str(getenv('MULTI_TOKEN2', '6213184839:AAGDu5TrR6ZiUPB9n82e6z_Anb_b6gMr_mA'))
     name = str(getenv('name', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', "-1001754309185"))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-100175430918'))
     PORT = int(getenv('PORT', 80))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5104293442").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', "Ipapcorn_helper"))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'Ipapcorn_helper'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
     
     else:
         ON_HEROKU = False
-    FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
+    FQDN = str(getenv('FQDN', '143.110.250.241')) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', "mongodb+srv://W9Uyj61afEXSw601:W9Uyj61afEXSw601@cluster0.zsxso.mongodb.net/?retryWrites=true&w=majority"))
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://W9Uyj61afEXSw601:W9Uyj61afEXSw601@cluster0.zsxso.mongodb.net/?retryWrites=true&w=majority'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
